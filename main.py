42d5f90a82b5d56d7fb5f87ac6bf7088692f67fc
WenboKou <kouwenbocn@163.com>
2024-08-13 00:10:40 +0800

    imitate SupervisedDataSet

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Dict
 
 import torch
 import transformers
+from torch.utils.data import Dataset
 from transformers.trainer_pt_utils import LabelSmoother
 
 
@@ -31,3 +32,25 @@ def preprocess(
     return dict(
         input_ids=input_ids, target_ids=target_ids, attention_mask=attention_mask
     )
+
+
+class SupervisedDataset(Dataset):
+    def __init__(self, raw_data, tokenizer: transformers.PreTrainedTokenizer, max_len: int):
+        super().__init__()
+
+        messages = [sample["messages"] for sample in raw_data]
+        data_dict = preprocess(messages, tokenizer, max_len)
+
+        self.input_ids = data_dict["input_ids"]
+        self.target_ids = data_dict["target_ids"]
+        self.attention_mask = data_dict["attention_mask"]
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, i) -> Dict[str, torch.Tensor]:
+        return dict(
+            input_ids=self.input_ids[i],
+            target_ids=self.target_ids[i],
+            attention_mask=self.attention_mask[i]
+        )
